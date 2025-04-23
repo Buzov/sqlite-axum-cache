63964f9737de842f190bc04b43a82128e246d898
Artur <buzov.artur@gmail.com>
2025-04-23 18:20:14 +0200

    refactoring of logging

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ use dotenvy::dotenv;
 use std::env;
 use application::AppState;
 use database::init_db;
-use crate::database::{delete_old_records, DbPool};
 use time::TimeUnit;
+use tracing::{info};
 use crate::application::create_app;
-use tracing_appender::rolling;
-use tracing_subscriber::fmt::writer::MakeWriterExt;
-use tracing::{info, Level};
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use crate::database::{delete_old_records, DbPool};
+use crate::logging::setup_logging;
+
 mod entity;
 mod handlers;
 mod database;
 mod time;
 mod application;
+mod logging;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -38,32 +38,6 @@ async fn main() -> anyhow::Result<()> {
     axum::serve(listener, app).await?;
 
     Ok(())
-}
-
-fn setup_logging() {
-    let debug_level = env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
-
-    // File appender: daily logs to ./logs/app.log
-    let file_appender = rolling::daily("./logs", "app.log")
-        .with_max_level(Level::INFO);
-
-    // Logging layer for file
-    let file_layer = fmt::layer()
-        .with_writer(file_appender)
-        .with_ansi(false) // optional: disables ANSI colors in log file
-        .with_level(true);
-
-    // Logging layer for stdout
-    let console_layer = fmt::layer()
-        .with_ansi(true)
-        .with_level(true); // shows the log level (INFO, DEBUG, etc.)
-
-    // Set up tracing
-    tracing_subscriber::registry()
-        .with(EnvFilter::new(debug_level)) // enables filtering by RUST_LOG
-        .with(file_layer)
-        .with(console_layer)
-        .init();
 }
 
 fn init_schedule_task(db_pool: DbPool) {
